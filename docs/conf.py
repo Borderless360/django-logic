5995c60a6fb3953c39a96ca273fd179dfa5c8278
Emil <emil.balashov.1@gmail.com>
2019-12-23 14:52:41 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.graphviz',
 ]
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
